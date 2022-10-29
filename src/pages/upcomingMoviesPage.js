bdf3fd3fbde857b6b9509b6ad2f2d444426c6187
Sadge-Student <20092443@mail.wit.ie>
2022-10-29 01:33:18 +0100

    New button for upcoming movies page.

--- src/pages/upcomingMoviesPage.js
+++ src/pages/upcomingMoviesPage.js
@@ -4,19 +4,9 @@ import {getMovies, getUpcomingMovies} from "../api/tmdb-api"
 import { useQuery } from "react-query";
 import Spinner from "../components/spinner"
 import AddToFavouritesIcon from "../components/cardIcons/addToFavourites"
+import PlaylistAddIcon from "@mui/icons-material/PlaylistAdd"
 
 const UpcomingMoviesPage = (props) => {
-  // const [movies, setMovies] = useState([]);
-  // const favourites = movies.filter((m) => m.favourite);
-  // localStorage.setItem("favourites", JSON.stringify(favourites));
-
-  // const addToFavourites = (movieId) => {
-  //   const updatedMovies = movies.map((m) =>
-  //     m.id === movieId ? { ...m, favourite: true } : m
-  //   );
-  //   setMovies(updatedMovies);
-  // };
-
   const { data, error, isLoading, isError } = useQuery('upcoming', getUpcomingMovies)
 
   if (isLoading)
@@ -35,7 +25,7 @@ const UpcomingMoviesPage = (props) => {
       title="Upcoming Movies"
       movies={movies}
       action={(movie) => {
-        return <AddToFavouritesIcon movie={movie} />
+        return <PlaylistAddIcon />
       }}
     />
   );
