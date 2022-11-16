cbe7c188c2194b9939b4f45bc6ef226f27c67ea7
Sadge-Student <20092443@mail.wit.ie>
2022-11-16 20:41:46 +0000

    Displays email when logged in and allows user to log out

--- src/components/siteHeader/index.js
+++ src/components/siteHeader/index.js
@@ -1,17 +1,19 @@
-import React, { useState } from "react"
-import AppBar from "@mui/material/AppBar"
-import ToolBar from "@mui/material/Toolbar"
-import Typography from "@mui/material/Typography"
-import IconButton from "@mui/material/IconButton"
-import Button from "@mui/material/Button"
-import MenuIcon from "@mui/material/Menu"
-import MenuItem from "@mui/material/MenuItem"
-import Menu from "@mui/material/Menu"
-import { useNavigate } from "react-router-dom"
-import { styled } from '@mui/material/styles'
-import { useTheme } from "@mui/material/styles"
-import { useMediaQuery } from "@mui/material"
+import React, { useState } from "react";
+import AppBar from "@mui/material/AppBar";
+import ToolBar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
+import Button from "@mui/material/Button";
+import MenuIcon from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import Menu from "@mui/material/Menu";
+import { useNavigate } from "react-router-dom";
+import { styled } from "@mui/material/styles";
+import { useTheme } from "@mui/material/styles";
+import { useMediaQuery } from "@mui/material";
+import Alert from "@mui/material/Alert";
 import { ThemeToggle} from "../themeToggle";
+import { useAuth } from "../../contexts/authContext";
 
 
 const Offset = styled('div')(({ theme }) => theme.mixins.toolbar);
@@ -19,11 +21,32 @@ const Offset = styled('div')(({ theme }) => theme.mixins.toolbar);
 const SiteHeader = ({ history }) => {
     const [anchorEl, setAnchorEl] = useState(null);
     const open = Boolean(anchorEl);
+    const [error, setError] = useState('');
+    const { currentUser, signout } = useAuth();
+    const navigate = useNavigate();
 
     const theme = useTheme();
     const isMobile = useMediaQuery(theme.breakpoints.down("md"));
 
-    const navigate = useNavigate();
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+      
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    async function handleLogout() {
+        setAnchorEl(null);
+        setError('');
+
+        try {
+            await signout();
+            navigate('/login');
+        } catch {
+            setError('Failed to log out.');
+        }
+    }
 
     const menuOptions = [
         { label: "Home", path: "/" },
@@ -100,6 +123,45 @@ const SiteHeader = ({ history }) => {
                         </>
                     )}
                     <ThemeToggle />
+                    
+                    {error && <Alert severity="error">{error}</Alert>}
+                    
+                    {currentUser !== null && 
+                        <div>
+                        <Button
+                        id="basic-button"
+                        aria-controls={open ? 'basic-menu' : undefined}
+                        aria-haspopup="true"
+                        aria-expanded={open ? 'true' : undefined}
+                        onClick={handleClick}
+                        >
+                        {currentUser.email}
+                        </Button>
+                        <Menu
+                        id="basic-menu"
+                        anchorEl={anchorEl}
+                        open={open}
+                        onClose={handleClose}
+                        MenuListProps={{
+                            'aria-labelledby': 'basic-button',
+                        }}
+                        >
+                        <MenuItem onClick={handleLogout}>Logout</MenuItem>
+                        </Menu>
+                    </div>
+                    }
+                    
+                    {currentUser === null &&
+                    <>
+                        <Button key="signin" color="inherit" href="/login">
+                            Sign In
+                        </Button>
+
+                        <Button key="register" color="inherit" href="/signup">
+                            Register
+                        </Button>
+                    </>
+                    }
                 </ToolBar>
             </AppBar>
             <Offset />
