5ce5cb2aa78f4cdae43a8bba249498db83b31595
Sadge-Student <20092443@mail.wit.ie>
2022-11-16 17:44:30 +0000

    Commented out lab routes to build firebase implementation, moved theme control here

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from "react"
 import {createRoot} from "react-dom/client"
 import { QueryClientProvider, QueryClient } from "react-query"
 import { ReactQueryDevtools } from "react-query/devtools"
-import { BrowserRouter, Route, Navigate, Routes } from "react-router-dom"
+// import { BrowserRouter, Route, Navigate, Routes } from "react-router-dom"
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom"
 import MoviesContextProvider from "./contexts/moviesContext"
 import HomePage from "./pages/homePage"
 import MoviePage from "./pages/movieDetailsPage"
@@ -11,6 +12,10 @@ import MovieReviewPage from "./pages/movieReviewPage"
 import SiteHeader from "./components/siteHeader"
 import UpcomingMoviesPage from "./pages/upcomingMoviesPage"
 import AddMovieReviewPage from "./pages/addMovieReviewPage"
+import SignUpPage from "./pages/signupPage"
+import LoginPage from "./pages/loginPage"
+import { AuthProvider } from "./contexts/authContext"
+import { ToggleColorMode, ThemeToggle} from "./components/themeToggle";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -25,22 +30,40 @@ const queryClient = new QueryClient({
 const App = () => {
     return (
       <QueryClientProvider client={queryClient}>
-        <BrowserRouter>
-          <SiteHeader />
-          <MoviesContextProvider>
-            <Routes>
-              <Route path="/reviews/form" element={<AddMovieReviewPage />} />
-              <Route path="/reviews/:id" element={ <MovieReviewPage />} />
-              <Route exact path="/movies/favourites" element={<FavouriteMoviesPage />} />
-              <Route exact path="/movies/upcoming" element={<UpcomingMoviesPage />} />
-              <Route path="/movies/:id" element={<MoviePage />} />
-              <Route path="/" element={<HomePage />} />
-              <Route path="*" element={<Navigate to="/" />} />
-            </Routes>
-          </MoviesContextProvider>
-        </BrowserRouter>
-        <ReactQueryDevtools initialIsOpen={false} />
+          <Router>
+            <AuthProvider>
+              <ToggleColorMode>
+                <SiteHeader />
+                {/* <ThemeToggle /> */}
+                <MoviesContextProvider>
+                  <Routes>
+                    <Route exact path="/" element={<HomePage />} />
+                    <Route path="/signup" element={<SignUpPage />} />
+                    <Route path="/login" element={<LoginPage />} />
+                  </Routes>
+                </MoviesContextProvider>
+              </ToggleColorMode>
+            </AuthProvider>
+          </Router>
+      <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
+      // <QueryClientProvider client={queryClient}>
+      //   <BrowserRouter>
+      //     <SiteHeader />
+      //     <MoviesContextProvider>
+      //       <Routes>
+      //         <Route path="/reviews/form" element={<AddMovieReviewPage />} />
+      //         <Route path="/reviews/:id" element={ <MovieReviewPage />} />
+      //         <Route exact path="/movies/favourites" element={<FavouriteMoviesPage />} />
+      //         <Route exact path="/movies/upcoming" element={<UpcomingMoviesPage />} />
+      //         <Route path="/movies/:id" element={<MoviePage />} />
+      //         <Route path="/" element={<HomePage />} />
+      //         <Route path="*" element={<Navigate to="/" />} />
+      //       </Routes>
+      //     </MoviesContextProvider>
+      //   </BrowserRouter>
+      //   <ReactQueryDevtools initialIsOpen={false} />
+      // </QueryClientProvider>
     );
 };
 
