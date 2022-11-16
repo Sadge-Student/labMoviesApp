07febddde89ebc62f1a74d2679eb7d6a8adbfda8
Sadge-Student <20092443@mail.wit.ie>
2022-11-16 17:36:03 +0000

    moved dark mode to global, added firebase account creation implementation, linked signin page

--- src/components/signup/index.js
+++ src/components/signup/index.js
@@ -1,18 +1,18 @@
-import React from "react";
+import React, { useState} from "react";
 import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
 import CssBaseline from "@mui/material/CssBaseline";
 import TextField from "@mui/material/TextField";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import Checkbox from "@mui/material/Checkbox";
 import Link from "@mui/material/Link";
 import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
+import Alert from "@mui/material/Alert";
+import AlertTitle from "@mui/material/AlertTitle";
 import { useAuth } from "../../contexts/authContext";
+import { useNavigate } from "react-router-dom";
 
 function Copyright(props) {
     return (
@@ -27,29 +27,41 @@ function Copyright(props) {
     );
   }
   
-  const theme = createTheme();
-  
   export default function SignUp() {
-    const { signup } = useAuth();
+    const { signup, currentUser } = useAuth();
+    const [error, setError] = useState('');
+    const [loading, setLoading] = useState(false);
+    const navigate = useNavigate();
 
-    const handleSubmit = (event) => {
+    async function handleSubmit(event) {
       event.preventDefault();      
       const data = new FormData(event.currentTarget);
 
       if (data.get('password') !== data.get('password-confirm')) {
+        return setError("Passwords do not match");
       }
+
+      try {
+        setError('');
+        setLoading(true);
+        await signup(data.get('email'), data.get('password'));
+        navigate("/");
+      } catch(e) {
+        console.log(e);
+        setError("Account creation failed");
+      };
+      setLoading(false);
 
       console.log({
         email: data.get('email'),
         password: data.get('password'),
         confirmPassword: data.get('password-confirm'),
       });
-    //   signup(data.get('email'), data.get('password'))
     };
   
     return (
-      <ThemeProvider theme={theme}>
         <Container component="main" maxWidth="xs">
+            {/* {JSON.stringify(currentUser)} */}
           <CssBaseline />
           <Box
             sx={{
@@ -65,8 +77,21 @@ function Copyright(props) {
             <Typography component="h1" variant="h5">
               Sign up
             </Typography>
-            <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+            
+            <Box component="form" onSubmit={handleSubmit} sx={{ mt: 3 }}>
+
               <Grid container spacing={2}>
+
+              {
+                error && 
+                <Grid item xs={12}>
+                    <Alert severity="error">
+                        <AlertTitle>Error</AlertTitle>
+                        {error}
+                    </Alert>
+                </Grid>
+                }
+
                 <Grid item xs={12}>
                   <TextField
                     required
@@ -104,13 +129,14 @@ function Copyright(props) {
                 type="submit"
                 fullWidth
                 variant="contained"
+                disabled={loading}
                 sx={{ mt: 3, mb: 2 }}
               >
                 Sign Up
               </Button>
               <Grid container justifyContent="flex-end">
                 <Grid item>
-                  <Link href="#" variant="body2">
+                  <Link href="/signin" variant="body2">
                     Already have an account? Sign in
                   </Link>
                 </Grid>
@@ -119,6 +145,5 @@ function Copyright(props) {
           </Box>
           <Copyright sx={{ mt: 5 }} />
         </Container>
-      </ThemeProvider>
     );
   }
