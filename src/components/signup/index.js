392010c26dd1acb71c11147585b62a48d9a22efb
Sadge-Student <20092443@mail.wit.ie>
2022-11-17 23:19:56 +0000

    fix login url

--- src/components/signup/index.js
+++ src/components/signup/index.js
@@ -136,7 +136,7 @@ function Copyright(props) {
               </Button>
               <Grid container justifyContent="flex-end">
                 <Grid item>
-                  <Link href="/signin" variant="body2">
+                  <Link href="/login" variant="body2">
                     Already have an account? Sign in
                   </Link>
                 </Grid>
