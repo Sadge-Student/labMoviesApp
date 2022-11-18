c103f6917402de0f992d3384b8cbb532e9d113f2
Sadge-Student <20092443@mail.wit.ie>
2022-11-18 01:27:15 +0000

    refactoring

--- src/components/privateRoute/index.js
+++ src/components/privateRoute/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Navigate, Outlet } from "react-router-dom";
-import { useAuth } from "../../contexts/authContext"
+import { useAuth } from "../../contexts/authContext";
 
 export default function PrivateRoute() {
     const { currentUser } = useAuth();
