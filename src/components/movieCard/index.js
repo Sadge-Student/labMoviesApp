0d45d45471a2a6a5b28c56a97000fd8455ad253d
Sadge-Student <20092443@mail.wit.ie>
2022-11-18 22:04:14 +0000

    Updated cards with parallax effect and colour adjustments

--- src/components/movieCard/index.js
+++ src/components/movieCard/index.js
@@ -11,10 +11,10 @@ import Typography from "@mui/material/Typography"
 import FavoriteIcon from "@mui/icons-material/Favorite"
 import CalendarIcon from "@mui/icons-material/CalendarTodayTwoTone"
 import StarRateIcon from "@mui/icons-material/StarRate"
-import IconButton from "@mui/material/IconButton"
 import Grid from "@mui/material/Grid"
 import { MoviesContext } from "../../contexts/moviesContext"
 import img from '../../images/film-poster-placeholder.png'
+import Tilt from "react-parallax-tilt";
 
 export default function MovieCard({movie, action}) {
   const { favourites, addToFavourites } = useContext(MoviesContext);
@@ -32,47 +32,65 @@ export default function MovieCard({movie, action}) {
   
   return (
     <Card sx={{ maxWidth: 345 }}>
-      <CardHeader 
-        avater={
-          movie.favourite ? (
-            <Avatar sx={{ backgroundColor: 'red' }}>
-              <FavoriteIcon />
-            </Avatar>
-          ) : null
-        }
-        title={
-          <Typography variant="h5" component="p">
-            {movie.title}{" "}
-          </Typography>
-        }
-        />
-      <CardMedia
-        sx={{ height: 500 }}
-        image={
-          movie.poster_path
-            ? `https://image.tmdb.org/t/p/w500/${movie.poster_path}`
-            : img
-        }
-      />
-      <CardContent>
-        <Grid container>
-          <Grid item xs={6}>
-            <Typography variant="h6" component="p">
-              <CalendarIcon fontSize="small" />
-              {movie.release_date}
+      <Tilt className="parallax-effect-glare-scale"
+            perspective={500}
+            glareEnable={true}
+            glareMaxOpacity={0.45}
+            transitionSpeed={1500}
+      >
+        <CardHeader 
+          avater={
+            movie.favourite ? (
+              <Avatar sx={{ backgroundColor: 'red' }}>
+                <FavoriteIcon />
+              </Avatar>
+            ) : null
+          }
+          title={
+            <Typography variant="h5" component="p"
+            sx={{
+              overflow: "hidden",
+              textOverflow: "ellipsis",
+              display: "-webkit-box",
+              WebkitLineClamp: "1",
+              WebkitBoxOrient: "vertical",
+              // height: "64.3px"
+            }}
+            >
+              {movie.title}{" "}
             </Typography>
+          }
+          />
+          <Link to={`/movies/${movie.id}`}>
+        <CardMedia
+          sx={{ height: 400 }}
+          image={
+            movie.poster_path
+              ? `https://image.tmdb.org/t/p/w500/${movie.poster_path}`
+              : img
+          }
+          />
+        </Link>
+        <CardContent>
+          <Grid container>
+            <Grid item xs={8}>
+              <Typography variant="h6" component="p">
+                <CalendarIcon fontSize="small" />
+                {movie.release_date}
+              </Typography>
+            </Grid>
+            <Grid item xs={4}>
+              <Typography variant="h6" component="p">
+                <StarRateIcon fontSize="small" sx={{color: "gold"}} />
+                {"  "} {parseFloat(movie.vote_average).toFixed(1)}{" "}
+              </Typography>
+            </Grid>
           </Grid>
-          <Grid item xs={6}>
-            <Typography variant="h6" component="p">
-              <StarRateIcon fontSize="small" />
-              {"  "} {movie.vote_average}{" "}
-            </Typography>
-          </Grid>
-        </Grid>
-      </CardContent>
+        </CardContent>
+      </Tilt>
       <CardActions disableSpacing>
         {action(movie)}
-        <Link to={`/movies/${movie.id}`}>
+        <Link to={`/movies/${movie.id}`} style={{ textDecoration: 'none' }}>
           <Button variant="outlined" size="medium" color="primary">
             More Info ...
           </Button>
