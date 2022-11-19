e6a01b1b338467fd74d19bd7452757e1480f47f3
Sadge-Student <20092443@mail.wit.ie>
2022-11-19 01:23:36 +0000

    Created top-rated movies page

---
+++ src/pages/ratedMoviesPage.js
@@ -0,0 +1,32 @@
+import React from "react"
+import PageTemplate from "../components/templateMovieListPage"
+import {getMovies, getTopRatedMovies} from "../api/tmdb-api"
+import { useQuery } from "react-query";
+import Spinner from "../components/spinner"
+import AddToFavouritesIcon from "../components/cardIcons/addToFavourites";
+
+const RatedMoviesPage = (props) => {
+  const { data, error, isLoading, isError } = useQuery('top_rated', getTopRatedMovies)
+
+  if (isLoading)
+    return <Spinner />;
+
+  if (isError)
+    return <h1>{error.message}</h1>;
+
+  const movies = data.results;
+  const favourites = movies.filter(m => m.favourite)
+  localStorage.setItem('favourites', JSON.stringify(favourites))
+  const addToFavourites = (movidId) => true
+
+  return (
+    <PageTemplate
+      title="Upcoming Movies"
+      movies={movies}
+      action={(movie) => {
+        return <AddToFavouritesIcon movie={movie} />
+      }}
+    />
+  );
+};
+export default RatedMoviesPage;
