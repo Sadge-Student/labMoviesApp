262455e62beed75297705ee1726b5a8326ec0485
Sadge-Student <20092443@mail.wit.ie>
2022-11-19 15:43:12 +0000

    Added actor favourite icon component

---
+++ src/components/cardIcons/addToActorFavourites.js
@@ -0,0 +1,20 @@
+import React, { useContext } from "react"
+import { ActorsContext } from "../../contexts/actorsContext"
+import IconButton from "@mui/material/IconButton"
+import FavoriteIcon from "@mui/icons-material/Favorite"
+
+const AddToFavouritesIcon = ({ actor }) => {
+    const context = useContext(ActorsContext);
+
+    const handleAddToFavourites = (e) => {
+        e.preventDefault();
+        context.addToFavourites(actor);
+    };
+
+    return (
+        <IconButton aria-label="add to favourites" onClick={handleAddToFavourites}>
+            <FavoriteIcon color="primary" fontSize="large" />
+        </IconButton>
+    );
+};
+export default AddToFavouritesIcon;
