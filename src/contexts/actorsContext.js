a5e83ebb4537417aeff0dc92b3e4407592e1e147
Sadge-Student <20092443@mail.wit.ie>
2022-11-19 15:45:34 +0000

    Created an Actors Context Provider

---
+++ src/contexts/actorsContext.js
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from "react";
+import { db } from "../firebase";
+import { query, collection, onSnapshot, updateDoc, doc, addDoc } from "firebase/firestore";
+import { useAuth } from "./authContext";
+
+export const ActorsContext = React.createContext(null);
+
+const ActorsContextProvider = (props) => {
+    const [favourites, setFavourites] = useState([]);
+    const { signin, currentUser } = useAuth();
+
+    const addFavouriteSync = async(actorId) => {
+        console.log("attempting to add " + actorId + " to firebase");
+        await addDoc(collection(db, `${currentUser.uid}/favourites/favouriteActors`), {
+            text: actorId
+        });
+    };
+
+    const addToFavourites = (actor) => {        
+        if (!favourites.includes(actor.id)) {
+            addFavouriteSync(actor.id);
+        }
+    };
+
+    const removeFromFavourites = (actor) => {
+        setFavourites(favourites.filter(
+            (aId) => aId !== actor.id
+        ))
+    };
+
+    return (
+        <ActorsContext.Provider
+            value={{
+                favourites,
+                addToFavourites,
+                removeFromFavourites,
+            }}
+        >
+            {props.children}
+        </ActorsContext.Provider>
+    );
+};
+export default ActorsContextProvider;
