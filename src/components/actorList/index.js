00ba12f620d4351a3ecc787e9204815468f6d5c2
Sadge-Student <20092443@mail.wit.ie>
2022-11-20 18:35:45 +0000

    Bugfix: actions are now defined

--- src/components/actorList/index.js
+++ src/components/actorList/index.js
@@ -2,8 +2,8 @@ import React from "react";
 import Actor from "../actorCard";
 import Grid from "@mui/material/Grid";
 
-const ActorList = (actors, action) => {
-    let actorCards = actors.actors.map((a)  => (
+const ActorList = ({actors, action}) => {
+    let actorCards = actors.map((a)  => (
         <Grid key={a.id} item xs={12} sm={6} md={4} lg={3} xl={2}>
             <Actor key={a.id} actor={a} action={action} />
         </Grid>
