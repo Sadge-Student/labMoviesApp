088d098074519fa3e6f861bf9b71f077d9575215
Sadge-Student <20092443@mail.wit.ie>
2022-11-21 06:40:56 +0000

    Created remove button/icon component for must watch

---
+++ src/components/cardIcons/removeFromMustWatch.js
@@ -0,0 +1,23 @@
+import React, { useContext } from "react"
+import IconButton from "@mui/material/IconButton"
+import DeleteIcon from "@mui/icons-material/Delete"
+import { MoviesContext } from "../../contexts/moviesContext"
+
+const RemoveFromMustWatchIcon = ({ movie }) => {
+    const context = useContext(MoviesContext);
+
+    const handleRemoveFromFavourites = (e) => {
+        e.preventDefault();
+        context.removeFromFavourites(movie);
+    };
+
+    return (
+        <IconButton
+            aria-labelledby="remove from favourites"
+            onClick={handleRemoveFromFavourites}
+        >
+            <DeleteIcon color="primary" fontSize="large" />
+        </IconButton>
+    );
+};
+export default RemoveFromMustWatchIcon;
