f5d6888613ddb97c170daa814ef8225136f7e750
Sadge-Student <20092443@mail.wit.ie>
2022-11-22 03:24:22 +0000

    Created favourite actors page that display actors you mark as favourites

---
+++ src/pages/favouriteActorsPage.js
@@ -0,0 +1,67 @@
+import React, { useEffect, useState } from "react";
+import PageTemplate from "../components/templateActorsPage";
+import { useQueries } from "react-query";
+import { getActor } from "../api/tmdb-api";
+import Spinner from "../components/spinner";
+import { query, collection, onSnapshot } from "firebase/firestore";
+import { db } from "../firebase";
+import { useAuth } from "../contexts/authContext";
+import RemoveFromFavouriteActor from "../components/cardIcons/removeFromFavouriteActor";
+
+const FavouriteActorsPage = () => {
+    const [favourites, setFavourites] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const { signin, currentUser } = useAuth();
+
+    useEffect(() => {
+        const q = query(collection(db, `${currentUser.uid}/favourites/actors`));
+        const unsubscribe = onSnapshot(q, (querySnapshot) => {
+            let favouritesArr = [];
+            querySnapshot.forEach((doc) => {
+                favouritesArr.push({...doc.data()});
+            });
+            setFavourites(favouritesArr);
+        });
+        setLoading(false);
+        return () => unsubscribe();
+    }, []);
+
+    const favouriteActorQueries = useQueries(
+        favourites.map((actorId) => {
+            return {
+                queryKey: ["actor", { id: Object.values(actorId) }],
+                queryFn: getActor,
+            };
+        })
+    );
+
+    const isLoading = favouriteActorQueries.find((a) => a.isLoading === true);
+
+    if (isLoading) {
+        return <Spinner />
+    }
+
+    const actors = favouriteActorQueries.map((q) => {
+        // console.log(q.data);
+        return q.data;
+    }) 
+
+    return (
+        <>
+        {!loading && 
+        <PageTemplate
+        title="Favourite Actors"
+        actors={actors}
+        action={(actor) => {
+            return (
+                <>
+                    <RemoveFromFavouriteActor actor={actor} />
+                </>
+            )
+        }}
+        />
+        }
+        </>
+    )
+};
+export default FavouriteActorsPage;
