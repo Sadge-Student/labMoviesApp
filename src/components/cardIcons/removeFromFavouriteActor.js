6661922934d6ff9d0f08b37ed2b025af6ce75287
Sadge-Student <20092443@mail.wit.ie>
2022-11-23 12:21:17 +0000

    Added tooltip

--- src/components/cardIcons/removeFromFavouriteActor.js
+++ src/components/cardIcons/removeFromFavouriteActor.js
@@ -1,7 +1,8 @@
 import React, { useContext } from "react";
+import { ActorsContext } from "../../contexts/actorsContext";
 import IconButton from "@mui/material/IconButton";
 import DeleteIcon from "@mui/icons-material/Delete";
-import { ActorsContext } from "../../contexts/actorsContext";
+import Tooltip from "@mui/material/Tooltip";
 
 const RemoveFromFavouriteActorIcon = ({ actor }) => {
     const context = useContext(ActorsContext);
@@ -12,12 +13,11 @@ const RemoveFromFavouriteActorIcon = ({ actor }) => {
     };
 
     return (
-        <IconButton
-            aria-labelledby="remove from favourites"
-            onClick={handleRemoveFromFavourites}
-        >
-            <DeleteIcon color="primary" fontSize="large" />
-        </IconButton>
+        <Tooltip title="Remove Favourite" placement="bottom" arrow>
+            <IconButton aria-labelledby="remove from favourites" onClick={handleRemoveFromFavourites}>
+                <DeleteIcon color="primary" fontSize="large" />
+            </IconButton>
+        </Tooltip>
     );
 };
 export default RemoveFromFavouriteActorIcon;
