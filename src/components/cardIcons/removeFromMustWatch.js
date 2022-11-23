4fa781cc97a99938dfa82d6fe081fab2967f9e7a
Sadge-Student <20092443@mail.wit.ie>
2022-11-23 12:22:17 +0000

    Added tooltip

--- src/components/cardIcons/removeFromMustWatch.js
+++ src/components/cardIcons/removeFromMustWatch.js
@@ -2,6 +2,7 @@ import React, { useContext } from "react";
 import { MoviesContext } from "../../contexts/moviesContext";
 import IconButton from "@mui/material/IconButton";
 import DeleteIcon from "@mui/icons-material/Delete";
+import Tooltip from "@mui/material/Tooltip";
 
 const RemoveFromMustWatchIcon = ({ movie }) => {
     const context = useContext(MoviesContext);
@@ -12,12 +13,11 @@ const RemoveFromMustWatchIcon = ({ movie }) => {
     };
 
     return (
-        <IconButton
-            aria-labelledby="remove from favourites"
-            onClick={handleRemoveFromMustWatch}
-        >
-            <DeleteIcon color="primary" fontSize="large" />
-        </IconButton>
+        <Tooltip title="Remove From Must Watch" placement="bottom" arrow>
+            <IconButton aria-labelledby="remove from favourites" onClick={handleRemoveFromMustWatch}>
+                <DeleteIcon color="primary" fontSize="large" />
+            </IconButton>
+        </Tooltip>
     );
 };
 export default RemoveFromMustWatchIcon;
