a119b8c6237c855fe0b0078c5ebb93a7f7bc746f
Sadge-Student <20092443@mail.wit.ie>
2022-11-23 16:08:43 +0000

    Added Spoken Languages, Production Companies, new page styling

--- src/components/movieDetails/index.js
+++ src/components/movieDetails/index.js
@@ -8,7 +8,14 @@ import NavigationIcon from "@mui/icons-material/Navigation";
 import Fab from "@mui/material/Fab";
 import Typography from "@mui/material/Typography";
 import Drawer from "@mui/material/Drawer";
+import Button from "@mui/material/Button";
+import LinkIcon from "@mui/icons-material/Link";
 import MovieReviews from "../movieReviews";
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+import CardMedia from "@mui/material/CardMedia";
+import img from "../../images/no-image.png";
+import Grid from "@mui/material/Grid";
 
 
 const root = {
@@ -22,6 +29,7 @@ const root = {
 const chip = { margin: 0.5 };
 
 const MovieDetails = ({ movie }) => {
+  console.log(movie);
   const [drawerOpen, setDrawerOpen] = useState(false);
 
   return (
@@ -34,10 +42,7 @@ const MovieDetails = ({ movie }) => {
         {movie.overview}
       </Typography>
 
-      <Paper 
-        component="ul" 
-        sx={root}
-      >
+      <Paper component="ul" sx={root} >
         <li>
           <Chip label="Genres" sx={chip} color="primary" />
         </li>
@@ -47,33 +52,88 @@ const MovieDetails = ({ movie }) => {
           </li>
         ))}
       </Paper>
+
       <Paper component="ul" sx={root}>
-        <Chip icon={<AccessTimeIcon />} label={`${movie.runtime} min.`} sx={chip}/>
-        <Chip
-          icon={<MonetizationIcon />}
-          label={`${movie.revenue.toLocaleString()}`}
-          sx={chip}
-        />
-        <Chip
-          icon={<StarRate />}
-          label={`${movie.vote_average} (${movie.vote_count}`}
-          sx={chip}
-        />
+        <Chip icon={<AccessTimeIcon />} label={`${movie.runtime} min`} sx={chip}/>
+        <Chip icon={<MonetizationIcon />} label={`${movie.revenue.toLocaleString()}`} sx={chip} />
+        <Chip icon={<StarRate />} label={`${movie.vote_average} (${movie.vote_count})`} sx={chip} />
         <Chip label={`Released: ${movie.release_date}`} sx={chip}/>
       </Paper>
-      <Paper 
-        component="ul" 
-        sx={root}
-      >
+      
+      <Paper component="ul" sx={root}>
         <li>
           <Chip label="Production Countries" sx={chip} color="primary" />
         </li>
+
         {movie.production_countries.map((c) => (
           <li key={c.name}>
             <Chip label={c.name} sx={chip} />
           </li>
         ))}
       </Paper>
+
+      <Paper component="ul" sx={root}>
+        <li>
+          <Chip label="Spoken Languages" sx={chip} color="primary" />
+        </li>
+        {movie.spoken_languages.map((l) => (
+            <li key={l.english_name}>
+              <Chip label={l.english_name} sx={chip} />
+            </li>
+        ))}
+      </Paper>
+
+      <Paper component="ul" sx={root}>
+        <Typography sx={{ fontSize: 18, m: 1 }} variant="subtitle1" gutterBottom>
+          Links
+        </Typography>
+        <a target="_blank" rel="noreferrer" href={`https://www.imdb.com/title/${movie.imdb_id}`} style={{textDecoration: 'none', display: 'contents'}}>
+          <Button sx={{maxWidth: '8%'}} variant="contained" endIcon={<LinkIcon />}>IMDB</Button>
+        </a>
+      </Paper>
+
+      <Paper component="ul" sx={{ p: 0}}>
+        <Grid container sx={{ m: 0 }}>
+          <Grid item xs={12}>
+            <Typography variant="h5" component="h3">
+              Production Companies
+            </Typography>
+          </Grid>
+          <Grid item container spacing={5} sx={{ m: 0}} style={{justifyContent: 'center'}}>
+            {movie.production_companies.map((pc) => (
+              <Card key={pc.id} sx={{ minWidth: 200, p: 1}}>
+                <CardHeader
+                  title={
+                    <Typography variant="h5" component="p"
+                      sx={{
+                      overflow: "hidden",
+                      textOverflow: "ellipsis",
+                      display: "-webkit-box",
+                      WebkitLineClamp: "1",
+                      WebkitBoxOrient: "vertical",
+                      }}
+                    >
+                      {pc.name}{" "}
+                    </Typography>
+                  }
+                />
+                <CardMedia
+                  sx={{ height: 200, minWidth: 200}}
+                  style={{
+                    backgroundSize: 'contain' 
+                  }}
+                  image={
+                      pc.logo_path
+                      ? `https://image.tmdb.org/t/p/w500/${pc.logo_path}`
+                      : img
+                  }
+                />
+              </Card>
+            ))}
+          </Grid>
+        </Grid>
+      </Paper>
+
       <Fab
         color="secondary"
         variant="extended"
