df20ae56a65087ca1876cda5c83a14d74f20a0b5
Sadge-Student <20092443@mail.wit.ie>
2022-11-23 12:20:08 +0000

    Added tooltip

--- src/components/cardIcons/addToFavourites.js
+++ src/components/cardIcons/addToFavourites.js
@@ -2,6 +2,7 @@ import React, { useContext } from "react";
 import { MoviesContext } from "../../contexts/moviesContext";
 import IconButton from "@mui/material/IconButton";
 import FavoriteIcon from "@mui/icons-material/Favorite";
+import Tooltip from "@mui/material/Tooltip";
 
 const AddToFavouritesIcon = ({ movie }) => {
     const context = useContext(MoviesContext);
@@ -12,9 +13,11 @@ const AddToFavouritesIcon = ({ movie }) => {
     };
 
     return (
-        <IconButton aria-label="add to favourites" onClick={handleAddToFavourites}>
-            <FavoriteIcon color="primary" fontSize="large" />
-        </IconButton>
+        <Tooltip title="Favourite Movie" placement="bottom" arrow>
+            <IconButton aria-label="add to favourites" onClick={handleAddToFavourites}>
+                <FavoriteIcon color="primary" fontSize="large" />
+            </IconButton>
+        </Tooltip>
     );
 };
 export default AddToFavouritesIcon;
