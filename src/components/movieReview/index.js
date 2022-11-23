ea04515f7d9cf82c25d2b7d6f4caa7fc634bbb88
Sadge-Student <20092443@mail.wit.ie>
2022-11-23 17:21:00 +0000

    Added more styling to full review page, Added markdown support, added author username

--- src/components/movieReview/index.js
+++ src/components/movieReview/index.js
@@ -1,10 +1,27 @@
 import React from "react";
+import ReactMarkdown from 'react-markdown';
+import Chip from "@mui/material/Chip";
+import Tooltip from '@mui/material/Tooltip';
+import Paper from "@mui/material/Paper";
+
+const chip = { margin: 0.5 };
 
 const MovieReview = ({ review }) => {
+    console.log(review);
     return (
         <>
-            <p>Review by: {review.author} </p>
-            <p>{review.content} </p>
+        <Paper component="ul" sx={{ p: '0 15px 15px 15px'}}>
+            <div style={{ padding: '25px 0 25px 0'}}>
+                <span> Review by: </span>
+                <Tooltip title="Author Name" arrow>
+                    <Chip label={review.author} sx={chip} color="primary" />
+                </Tooltip>
+                <Tooltip title="Username" arrow>
+                    <Chip label={`@${review.author_details.username}`} sx={chip} color="secondary" />
+                </Tooltip>
+            </div>
+            <ReactMarkdown>{review.content}</ReactMarkdown>
+        </Paper>
         </>
     );
 };
