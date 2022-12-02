e05496ed07daee33d1fd069d61ddd1e10bb44cd3
Sadge-Student <20092443@mail.wit.ie>
2022-12-02 02:24:46 +0000

    Added different actor card displays for favourite actors and most popular actors page since they return different actor api data

--- src/components/actorCard/index.js
+++ src/components/actorCard/index.js
@@ -15,6 +15,7 @@ import Tooltip from "@mui/material/Tooltip";
 import StarRateIcon from "@mui/icons-material/StarRate";
 import Button from "@mui/material/Button";
 import CardActions from "@mui/material/CardActions";
+import LinkIcon from "@mui/icons-material/Link";
 
 export default function ActorCard({actor, action}) {
     const { favourites, addToFavourites } = useContext(ActorsContext);
@@ -29,6 +30,7 @@ export default function ActorCard({actor, action}) {
         e.preventDefault();
         addToFavourites(actor);
     }
+
     return (
         <Card sx={{ maxWidth: 345}}>
             <Tilt className="parallax-effect-glare-scale"
@@ -71,28 +73,50 @@ export default function ActorCard({actor, action}) {
                     </Link>
                     </Tilt>
                     <CardContent>
-                    <Typography variant="overline" component="p">
-                        Known For
-                    </Typography>
-                    <Grid container style={{display: 'flex', alignItems: 'center', flexWrap: 'wrap', justifyContent: 'center'}}>
-                        <Grid item xs={8}>
-                            {actor.known_for.map((kf) => {
-                                return (
-                                    <Typography key={kf.id} variant="subtitle2" component="p">
-                                        {kf.title}
-                                    </Typography>
-                                );
-                            })}
-                        </Grid>
-                        <Grid item xs={4}>
-                        <Typography variant="h6" component="p">
-                            <Tooltip title="Popularity: based on number of views for the day" arrow>
-                                <StarRateIcon fontSize="small" sx={{color: "gold"}} />
-                            </Tooltip>
-                            {"  "} {parseFloat(actor.popularity).toFixed(1)}{" "}
+                    {actor.known_for !== undefined && <>
+                        <Typography variant="overline" component="p">
+                            Known For
                         </Typography>
+                        <Grid container style={{display: 'flex', alignItems: 'center', flexWrap: 'wrap', justifyContent: 'center'}}>
+                            <Grid item xs={8}>
+                                {actor.known_for.map((kf) => {
+                                    return (
+                                        <Typography key={kf.id} variant="subtitle2" component="p">
+                                            {kf.title}
+                                        </Typography>
+                                    );
+                                })}
+                            </Grid>
+                            <Grid item xs={4}>
+                            <Typography variant="h6" component="p">
+                                <Tooltip title="Popularity: based on number of views for the day" arrow>
+                                    <StarRateIcon fontSize="small" sx={{color: "gold"}} />
+                                </Tooltip>
+                                {"  "} {parseFloat(actor.popularity).toFixed(1)}{" "}
+                            </Typography>
+                            </Grid>
                         </Grid>
-                    </Grid>
+                    </>}
+                    {actor.known_for === undefined && <>
+                        <Typography variant="overline" component="p">
+                            Links
+                        </Typography>
+                        <Grid container style={{display: 'flex', flexWrap: 'wrap'}}>
+                            <Grid item xs={8}>
+                            <a target="_blank" rel="noreferrer" href={`https://www.imdb.com/title/${actor.imdb_id}`} style={{textDecoration: 'none', display: 'contents'}}>
+                                <Button sx={{minWidth: '10%'}} variant="contained" endIcon={<LinkIcon />}>IMDB</Button>
+                            </a>
+                            </Grid>
+                            <Grid item xs={4}>
+                            <Typography variant="h6" component="p" sx={{leftMargin: '66px', width: '100px'}}>
+                                <Tooltip title="Popularity: based on number of views for the day" arrow>
+                                    <StarRateIcon fontSize="small" sx={{color: "gold"}} />
+                                </Tooltip>
+                                {"  "} {parseFloat(actor.popularity).toFixed(1)}{" "}
+                            </Typography>
+                            </Grid>
+                        </Grid>
+                    </>}
                     </CardContent>
                     <CardActions disableSpacing>
                     {action(actor)}
