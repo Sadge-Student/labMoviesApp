56375d4203379017030f0e9ac940672bb1100de2
Sadge-Student <20092443@mail.wit.ie>
2022-12-02 03:04:38 +0000

    Removed pagination code and added extra props to templateMovieListPage

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -1,11 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useQuery } from "react-query";
 import { getMovies } from "../api/tmdb-api";
-import Grid from "@mui/material/Grid";
-import Button from "@mui/material/Button";
-import NavigateNextIcon from "@mui/icons-material/NavigateNext";
-import NavigateBeforeIcon from "@mui/icons-material/NavigateBefore";
-import TextField from "@mui/material/TextField";
 import PageTemplate from "../components/templateMovieListPage";
 import AddToFavouritesIcon from "../components/cardIcons/addToFavourites";
 import AddToMustWatch from "../components/cardIcons/addToMustWatch";
@@ -18,7 +13,7 @@ const HomePage = (props) => {
   useEffect(() => {
     refetch();
     topFunction();
-  }, [currentPage, refetch])
+  }, [currentPage, refetch]);
 
   function topFunction() {
     document.body.scrollTop = 0; // For Safari
@@ -33,42 +28,13 @@ const HomePage = (props) => {
 
   const movies = data.results;
 
-  // const favourites = movies.filter(m => m.favourite)
-  // localStorage.setItem('favourites', JSON.stringify(favourites))
-  // const addToFavourites = (movieId) => true
-
   return(
     <>
-    <div style={{paddingTop: '30px'}}>
-        <Grid container spacing={8}>
-          <Grid item xs={12}>
-            <Grid container justifyContent="center" spacing={2}>
-              <Button disabled={currentPage === 1} variant="outlined" startIcon={<NavigateBeforeIcon />} onClick={() => setCurrentPage(prevValue => prevValue - 1 )} style={{margin: '0 10px'}}>Previous Page</Button>
-              
-              <TextField 
-                id="standard-basic" 
-                label="Page Search" 
-                inputProps={{min: 0, style: { textAlign: 'center' }}}
-                variant="standard"
-                onKeyPress={(ev) => {
-                  if (ev.key === 'Enter') {
-                    if (!isNaN(ev.target.value) && ev.target.value <= 500 && ev.target.value > 0) {
-                      setCurrentPage(ev.target.value);
-                      ev.preventDefault();
-                    }
-                  }
-                }}
-              />
-              
-              <Button disabled={currentPage === 500} variant="outlined" endIcon={<NavigateNextIcon />} onClick={() => setCurrentPage(prevValue => prevValue + 1 )} style={{margin: '0 10px'}}>Next Page</Button>
-            </Grid>
-          </Grid>
-        </Grid>
-      </div>
-
        {!isLoading && <PageTemplate
-          title={`Discover Movies Page: ${currentPage}`}
+          title={`Discover Movies | Page: ${currentPage}`}
           movies={movies}
+          currentPage={currentPage}
+          setCurrentPage={setCurrentPage}
           action={(movie) => {
             return (
             <>
@@ -79,33 +45,6 @@ const HomePage = (props) => {
           }}
         />
       }
-
-      <div style={{padding: 20}}>
-        <Grid container spacing={8}>
-          <Grid item xs={12}>
-            <Grid container justifyContent="center" spacing={2}>
-              <Button disabled={currentPage === 1} variant="outlined" startIcon={<NavigateBeforeIcon />} onClick={() => setCurrentPage(prevValue => prevValue - 1 )} style={{margin: '0 10px'}}>Previous Page</Button>
-              
-              <TextField 
-                id="standard-basic" 
-                label="Page Search" 
-                inputProps={{min: 0, style: { textAlign: 'center' }}}
-                variant="standard"
-                onKeyPress={(ev) => {
-                  if (ev.key === 'Enter' && ev.target.value <= 500 && ev.target.value > 0) {
-                    if (!isNaN(ev.target.value)) {
-                      setCurrentPage(ev.target.value);
-                      ev.preventDefault();
-                    }
-                  }
-                }}
-              />
-              
-              <Button disabled={currentPage === 500} variant="outlined" endIcon={<NavigateNextIcon />} onClick={() => setCurrentPage(prevValue => prevValue + 1 )} style={{margin: '0 10px'}}>Next Page</Button>
-            </Grid>
-          </Grid>
-        </Grid>
-      </div>
     </>
   );
 };
