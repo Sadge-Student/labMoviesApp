fcffb1825ce8228ab587d8732369ac6524bec0f6
Sadge-Student <20092443@mail.wit.ie>
2022-12-04 21:41:42 +0000

    Created a fantasy template component

---
+++ src/components/templateFantasyMoviePage/index.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { useQuery } from "react-query";
+import { getActorImages } from "../../api/tmdb-api";
+import Grid from "@mui/material/Grid";
+import { Card, CardMedia } from "@mui/material";
+import Carousel from "react-material-ui-carousel";
+import FantasyMovieHeader from "../fantasyMovieHeader";
+import Spinner from "../spinner";
+
+const TemplateFantasyMoviePage = ({ movie, children }) => {
+
+    return (
+        <>
+          <FantasyMovieHeader movie={movie} />
+    
+          <Grid container spacing={5} sx={{ padding: "15px" }}>
+            <Grid item xs={4}>
+              {/* <Carousel>
+              { images.map( (image) => 
+                <Card key={image.file_path}>
+                  <CardMedia
+                      component="img"
+                      image={'https://image.tmdb.org/t/p/w500/' + image.file_path}
+                      alt={image.poster_path}
+                      height="500"
+                      title={image.file_path}
+                      style={{
+                          objectFit: "contain" 
+                      }}
+                  />
+                </Card>
+              ) }
+              </Carousel> */}
+            </Grid>
+    
+            <Grid item xs={8}>
+              {children}
+            </Grid>
+          </Grid>
+        </>
+    );
+}
+export default TemplateFantasyMoviePage;
